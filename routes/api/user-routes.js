dd34f0d783e4d5846808103456521c9bb2fd7617
Shane Wade <shanebramble96@hotmail.com>
2025-02-23 19:50:51 -0600

    created User model and sync with express server

---
+++ routes/api/user-routes.js
@@ -0,0 +1,106 @@
+const router = require("express").Router();
+const e = require("express");
+const { User } = require("../../models");
+
+// GET /api/users
+router.get("/", (req, res) => {
+  // Access our User model and run .findAll() method.
+  // The JavaScript equivalent of the following SQL query:
+  // SELECT * FROM users;
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET /api/users/1
+router.get("/:id", (req, res) => {
+  // The JavaScript equivalent to SELECT * FROM users WHERE id = 1;
+  User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post("/", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // The JavaScript equivalent to:
+  // INSERT INTO users (username, email, password)
+  // VALUES("Lernantino", "lernantino@gmail.com", "password1234");
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// PUT /api/users/1
+router.put("/:id", (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  // The associated SQL syntax would look like the following code:
+  // UPDATE users
+  // SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
+  // WHERE id = 1;
+
+  User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "User not found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE /api/users/1
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(500).json(err);
+    });
+});
+
+module.exports = router;
