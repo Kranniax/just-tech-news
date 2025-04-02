0824282fc3b146d45322ec7a53bc5542ea3b430b
Shane Wade <shanebramble96@hotmail.com>
2025-04-01 19:51:36 -0500

    completed the connection of the front-end to backend

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
+// get all posts for a logged in user.
 router.get("/", withAuth, (req, res) => {
   Post.findAll({
     where: {
@@ -45,12 +46,38 @@ router.get("/", withAuth, (req, res) => {
       res.status(500).json(err);
     });
 });
-
+// edit a posted route.
 router.get("/edit/:id", withAuth, (req, res) => {
   Post.findOne({
     where: {
       id: req.params.id,
     },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
   })
     .then((dbPostData) => {
       //serialized the data.
